867647351b851e2e8ce2df088111c5120476a23f
MINT <mint.2978@gmail.com>
2024-07-30 10:00:58 +0700

    feat: algorithm checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,16 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
-
-
+Answer: n เนื่องจากเป็น loop เดียวในการค้นหาข้อมูล และขนาดข้อมูลจะเพิ่มขึ้นตามจำนวนข้อมูล
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
-
-
+Answer: log n เนื่องจากมีการกำหนดขอบเขต left right และการแบ่งครึ่ง mid ทุกครั้ง/รอบในการเปรียบเทียบข้อมูล จะลดขนาดของข้อมูลที่ค้นหาครึ่งหนึ่ง
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: function findProductPrice เนื่องจากใช้เวลาในการค้นหาน้อยกว่า function findStudentById โดยเฉพาะเมื่อข้อมูลมีขนาดที่ใหญ่มากขึ้น
 
 
 */
